b64938e66f8ecf891b2546a538dd7a5dae2286f8
Chelsea Adetan <adetanchelsea@gmail.com>
2025-09-19 21:36:33 +0100

    Added extra code to the loading script.

--- scripts/load.py
+++ scripts/load.py
@@ -1,1 +1,11 @@
+"""
+YouTube Data Loading Script
 
+This script loads cleaned YouTube channel data into a Snowflake data warehouse. It ensures that transformed YouTube data is reliably stored in Snowflake
+for further analysis and reporting.
+
+"""
+
+import pandas as pd
+import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
